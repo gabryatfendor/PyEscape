48368912e411e1b65e11373651d8001319f45429
Gabriele Sani <gabriele.sani@antreem.com>
2019-03-20 15:01:01 +0100

    Printing file on screen

--- editor.py
+++ editor.py
@@ -7,7 +7,15 @@ def main():
     print("Levels currently loaded:")
     for level in levels:
         print(level.name)
+        print_level(level.level_path)
 
+def print_level(level_path):
+    with open(level_path, "r") as ins:
+        line_array = []
+        for line in ins:
+            line_array.append(line)
+    for line in line_array:
+        print(line)
 
 if __name__ == '__main__':
     main()
